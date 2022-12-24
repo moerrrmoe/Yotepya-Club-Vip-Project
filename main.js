2f7be7d1345ef2e988e5da89c942c7dc94e94629
moerrrmoe <92532476+moerrrmoe@users.noreply.github.com>
2022-12-24 23:20:48 +0630

    Update main.js

--- main.js
+++ main.js
@@ -12,25 +12,3 @@ var arr = [
 								exp: "11/23/2023"
 				}
 ]
-	
-	function loginFunction () {
- var today= new Date();
- var ok=0;
- var code = document.getElementById("myText1").value;
-
-        for(i = 0; i < arr.length; i++){
-            if(code == arr[i].code && today > new Date(arr[i].exp)){
-                var ok=1;
-            }
-        }
-        if(ok == 1){
-        				swal({title:"မင်္ဂလာပါ", text:"VIPအသုံးပြုသူခင်ဗျာ။", icon:"success"}).then((result) => {
-  if (result) {
-    window.location = "file:///storage/emulated/0/Download/zapya/misc/index.html"
-  }
-})
-        }
-        if(ok == 0){
- 												swal({title: "Error", text:"codeမှားယွင်း(သို့မဟုတ်)သက်တမ်းကုန်နေပါသည်", icon: "warning"})
- 								}    
-}
