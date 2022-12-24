4365a03798e85de57ffe0d3d195909109fa754b3
moerrrmoe <92532476+moerrrmoe@users.noreply.github.com>
2022-12-24 17:08:43 +0630

    Create main.js

---
+++ main.js
@@ -0,0 +1,1 @@
+var arr = [{username: 'Jon', genrePref: 'rock'},{username: 'Lucy', genrePref: 'pop'},{username: 'Mike', genrePref: 'rock'},{username: 'Luke', genrePref: 'house'},{username: 'James', genrePref: 'house'},{username: 'Dave', genrePref: 'bass'},{username: 'Sarah', genrePref: 'country'},{username: 'Natalie', genrePref: 'bass'}];
