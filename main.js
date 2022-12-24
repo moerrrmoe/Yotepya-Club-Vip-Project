78b84e0edd06cea8747e2f5ea87487cc63283c54
moerrrmoe <92532476+moerrrmoe@users.noreply.github.com>
2022-12-24 17:15:25 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var arr = [{username: 'Jon', genrePref: 'rock'},{username: 'Lucy', genrePref: 'pop'},{username: 'Mike', genrePref: 'rock'},{username: 'Luke', genrePref: 'house'},{username: 'James', genrePref: 'house'},{username: 'Dave', genrePref: 'bass'},{username: 'Sarah', genrePref: 'country'},{username: 'Natalie', genrePref: 'bass'}];
-var text = "<table border ='1'><tr><th>Username</th><th>genrePref</th></tr>"
+	
+	var text = "<table border ='1'><tr><th>Username</th><th>genrePref</th></tr>"
 	for (var i=0;i<arr.length;i++){
      text+="<tr><td>"+arr[i].username+"</td><td>"+arr[i].genrePref+"</td></tr>"				
 	}
