272da3c9f964531835de278411665e59ee9b4cc9
moerrrmoe <92532476+moerrrmoe@users.noreply.github.com>
2023-07-01 13:58:33 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var arr = [
 				{
-								code: "adpw",
-								exp: "7/13/2021"
+								code: "c61b21d75bed2ab1122b05325480fa96",
+								exp: "7/13/2024"
 				},
 				{
 								code:"7b631145fc6d0c9a051c727a48a09370",
